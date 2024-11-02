a485791c1395656449ca5f1d50403db99fc126c7
Ratapumin Naudom <ratapumin@gmail.com>
2024-11-02 22:18:50 +0700

    add put api

--- src/api/index.php
+++ src/api/index.php
@@ -2,11 +2,13 @@
 $uri = isset($_SERVER['REQUEST_URI']) ? parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) : null;
 $method = $_SERVER['REQUEST_METHOD'] ?? 'CLI';
 
-if ($uri === '/api/users' && $method === 'GET') {
+if ($uri === "/api/users" && $method === "GET") {
     require_once __DIR__ . '/users.php';
-} elseif ($uri === '/api/login' && $method === 'POST') {
-    require_once __DIR__ . '/users.php';
+} elseif ($uri === "/api/login" && $method === "POST") {
+    require_once __DIR__ . "/users.php";
 
+} elseif ($uri === "/api/update" && $method === "PUT") {
+    require_once __DIR__ . "/users.php";
 } else {
     echo json_encode(["message" => "ไม่พบเส้นทางที่ต้องการ"]);
 }
